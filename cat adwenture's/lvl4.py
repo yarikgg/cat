41dd719a0fb878ad4a6a91eaa9775206a0384d44
yarikgg <97395432+yarikgg@users.noreply.github.com>
2022-01-18 20:09:17 +0300

    catV1.1
    hey evryone i make the lvl 4 with sunset

--- cat adwenture's/lvl4.py
+++ cat adwenture's/lvl4.py
@@ -1,29 +1,28 @@
 import pygame
 import time
-pygame.init()
+
+
 w,h = 800,402
 display = pygame.display.set_mode((w,h))
 
-moster = pygame.image.load('water_monster.png')
 cat = pygame.image.load('cat.png')
 cat_animation1 = pygame.image.load('cat_animation1.png')
 cat_animation2 = pygame.image.load('cat_animation2.png')
 final_animation = pygame.image.load('cat_animation_final.png')
-bat = pygame.image.load('bat.png')
-lvl1 = pygame.image.load('lvl2.png')
+lvl1 = pygame.image.load('lvl4.png')
 mouse = pygame.image.load('mouse.png')
+chert = pygame.image.load('неадекват.png')
+fire_ball = pygame.image.load('fire ball.png')
 
 x = 0
 catx = 30
-caty = 325
+caty = 300
 
 mx = 600
 
-bx = 250
+fx=550
 
 stopmouse = True
-
-kx = 250
 
 i = 2
 
@@ -32,19 +31,19 @@ health = 3
 def graviti():
     global caty
     global catx
-    if caty <= 325:
+    if caty < 325:
         caty += 2
         if caty >= 325:
-            caty = 325
+            caty = 325 
 
 def graviti2():
     global caty
     global catx
     graviti = False
-    if caty <= 267:
+    if caty < 261:
         caty += 2
-        if caty >= 267:
-            caty = 267  
+        if caty >= 261:
+            caty = 261 
 
 display.blit(lvl1,(0,0))
 display.blit(cat,(catx,caty))
@@ -78,69 +77,73 @@ while start:
             if event.key == pygame.K_SPACE:
                 jump = False
 
-    f = pygame.font.SysFont('serif', 48)
-    text = f.render('здоровье='+str(health), True,
-        (0, 180, 0))
     clock.tick(FPS)
     if x == 1:
         catx += 2
     if x == -1:
         catx -= 2
-    if caty < 324 and catx >= 285 and catx <= 380:
-        jump=False
     if jump == True:
         display.blit(lvl1,(0,0))                        #   анимация
-        display.blit(cat_animation1,(catx,caty))
-        display.blit(text,(4,4))  
-        display.blit(moster,(kx,325))
-        display.blit(mouse,(mx, 355))      #   анимация
+        display.blit(cat_animation1,(catx,caty))  
+        display.blit(mouse,(mx, 287))   
+        display.blit(chert,(550, 250))
+        fx-=10
+        display.blit(fire_ball,(fx, 260))   #   анимация
         pygame.display.update()                         #   анимация   
         time.sleep(0.3)                                 #   анимация
         display.blit(lvl1,(0,0))                        #   анимация
         display.blit(cat_animation2,(catx,caty))
-        display.blit(text,(4,4))
-        display.blit(moster,(kx,325))
-        display.blit(mouse,(mx, 355))        #   анимация       
+        display.blit(mouse,(mx, 287))
+        display.blit(chert,(550, 250)) 
+        fx -= 10
+        display.blit(fire_ball,(fx, 260))       #   анимация       
         pygame.display.update()                         #   анимация
         time.sleep(0.3)
         display.blit(lvl1,(0,0))
-        caty -= 120
-        catx += 60
+        caty -= 140
+        catx += 100
         display.blit(final_animation,(catx,caty))
-        display.blit(text,(4,4))
-        display.blit(mouse,(mx, 355))
-        display.blit(moster,(kx,325))
+        display.blit(mouse,(mx, 287))
+        display.blit(chert,(550, 250))
+        fx -= 10
+        display.blit(fire_ball,(fx, 260))
         pygame.display.update()
         jump = False
-    bx -= 1
-    if catx <= 186:
+    if catx <= 73:
         graviti()
-    if catx >= 328:
-        graviti()
-    if catx >= 197 and catx <= 328:
+    if catx >= 74 and caty >= 262:
+        x = 0
+    if catx >= 74:
         graviti2()
     if catx >= 800:
         print('ты выйграл')
-        win = pygame.font.SysFont('serif', 48)
-        wintext = win.render('You Win!', True,
-                (180, 180, 0))
-        display.blit(wintext,(800//2,402//2))
         time.sleep(3)      
+        exit()
+    if catx >= 324 and catx <= 451:
+        caty += 2
+    if catx == 520 and 561 and caty >= 261:
+        health -= 1
+    if health == 0:
         exit()
     if catx == mx and 610 and jump == False:
         stopmouse = False
         health += 1
     if stopmouse == False:
         mx += 7
-    if catx >= 200 and catx <= 400 and caty >= 325 and jump == False:
-        caty += 3
-    if caty >= 330:
+    if caty >= 402:
         exit()
-    pygame.display.update()
+    if catx >= 141 and catx <= 240 and caty >= 261:
+        health -= 1
+    fx-=2
+    if fx <= 0:
+        fx=550
+    if fx == catx and caty == 261:
+        start = 0
     display.blit(lvl1,(0,0))        
     display.blit(cat,(catx,caty))    
-    display.blit(text,(4,4)) 
-    display.blit(moster,(kx,325))
-    display.blit(mouse,(mx, 355))          
+    display.blit(mouse,(mx, 287)) 
+    display.blit(chert,(550, 250))
+    display.blit(fire_ball,(fx, 260))
     pygame.display.update()
     
+
